0b5ade0904a40cc79cf812d8abaac48d888a6b43
VampGaming14 <65399588+VampGaming14@users.noreply.github.com>
2020-07-04 20:37:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:false});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  ellipse(345,364, 55, 55);
+   Matter.Bodies.circle(299,345, radius, [options], [maxSides])
+   World.add(world,circle);
+  drawSprites();
+ 
+}
+
+
+
